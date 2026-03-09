670ef177a906ac727e12df06d8a10c716efc349a
ampactor <morganespitia@gmail.com>
2026-03-09 01:18:44 -0600

    fix: set vite base for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  base: "/celezdial-selekta/",
   plugins: [react()],
   resolve: {
     alias: {
